57ca6a07000eef61bfd160ef56e24a6a119d7cad
Sergei Beilin <saabeilin@gmail.com>
2018-03-09 11:47:20 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def read(*names, **kwargs):
 
 setup(
     name='sanic-statsd',
-    version='0.1.1',
+    version='0.1.2',
     license='MIT license',
     description='A statsd plugin for Sanic',
     # long_description='%s\n%s' % (
